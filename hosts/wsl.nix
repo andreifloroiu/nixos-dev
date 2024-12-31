8a9149b1d04914d0500570c8fdc5ec26c06d80eb
Andrei Floroiu <floroiu.andrew@gmail.com>
2024-12-31 11:58:11 +0200

    Initial commit. Added source code. (#1)

---
+++ hosts/wsl.nix
@@ -0,0 +1,25 @@
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  imports = [
+    inputs.nixos-wsl.nixosModules.wsl
+    inputs.vscode-server.nixosModule
+  ];
+
+  wsl = {
+    enable = true;
+    defaultUser = "andrei";
+    docker-desktop = {
+      enable = true;
+    };
+  };
+
+  users = {
+    defaultUserShell = pkgs.zsh;
+  };
+
+  services.vscode-server.enable = true;
+
+  # Get latest from here https://github.com/nix-community/NixOS-WSL/releases
+  system.stateVersion = "24.05";
+}
