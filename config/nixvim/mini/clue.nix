0c229f21f46dc99c519480d64d8cad781deabfcd
Andrei Floroiu <floroiu.andrew@gmail.com>
2025-01-24 16:29:05 +0200

    More Mini Config
    - more mini config

--- config/nixvim/mini/clue.nix
+++ config/nixvim/mini/clue.nix
@@ -10,35 +10,35 @@
     #   miniclue.gen_clues.z(),
     # "";
     triggers = [
-        # Leader triggers
-        { mode = "n"; keys = "<Leader>"; }
-        { mode = "x"; keys = "<Leader>"; }
+      # Leader triggers
+      { mode = "n"; keys = "<Leader>"; }
+      { mode = "x"; keys = "<Leader>"; }
 
-        # Built-in completion
-        { mode = "i"; keys = "<C-x>"; }
+      # Built-in completion
+      { mode = "i"; keys = "<C-x>"; }
 
-        # `g` key
-        { mode = "n"; keys = "g"; }
-        { mode = "x"; keys = "g"; }
+      # `g` key
+      { mode = "n"; keys = "g"; }
+      { mode = "x"; keys = "g"; }
 
-        # Marks
-        { mode = "n"; keys = "\""; }
-        { mode = "n"; keys = "`"; }
-        { mode = "x"; keys = "\""; }
-        { mode = "x"; keys = "`"; }
+      # Marks
+      { mode = "n"; keys = "\""; }
+      { mode = "n"; keys = "`"; }
+      { mode = "x"; keys = "\""; }
+      { mode = "x"; keys = "`"; }
 
-        # Registers
-        { mode = "n"; keys = "\""; }
-        { mode = "x"; keys = "\""; }
-        { mode = "i"; keys = "<C-r>"; }
-        { mode = "c"; keys = "<C-r>"; }
+      # Registers
+      { mode = "n"; keys = "\""; }
+      { mode = "x"; keys = "\""; }
+      { mode = "i"; keys = "<C-r>"; }
+      { mode = "c"; keys = "<C-r>"; }
 
-        # Window commands
-        { mode = "n"; keys = "<C-w>"; }
+      # Window commands
+      { mode = "n"; keys = "<C-w>"; }
 
-        # `z` key
-        { mode = "n"; keys = "z"; }
-        { mode = "x"; keys = "z"; }
+      # `z` key
+      { mode = "n"; keys = "z"; }
+      { mode = "x"; keys = "z"; }
     ];
   };
 }
