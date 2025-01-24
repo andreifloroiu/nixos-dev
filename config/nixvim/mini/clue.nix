9f1327778c656161650f1e116a641a12bea9e33e
Andrei Floroiu <floroiu.andrew@gmail.com>
2025-01-24 09:58:57 +0200

    Configured Clue
    - configured mini.clue

--- config/nixvim/mini/clue.nix
+++ config/nixvim/mini/clue.nix
@@ -1,4 +1,18 @@
 { ... }: {
   programs.nixvim.plugins.mini.modules.clue = {
+    # clues = ''
+    #   -- Enhance this by adding descriptions for <Leader> mapping groups
+    #   miniclue.gen_clues.builtin_completion(),
+    #   miniclue.gen_clues.g(),
+    #   miniclue.gen_clues.marks(),
+    #   miniclue.gen_clues.registers(),
+    #   miniclue.gen_clues.windows(),
+    #   miniclue.gen_clues.z(),
+    # '';
+    triggers = [
+      { mode = "n"; keys = "<leader>"; }
+      { mode = "n"; keys = "g"; }
+      { mode = "n"; keys = "<C-w>"; }
+    ];
   };
 }
