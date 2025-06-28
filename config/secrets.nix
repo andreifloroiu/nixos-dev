932ccc7ccb6a6ecbf043abb96ef0d8e5340e7fcb
Andrei Floroiu <floroiu.andrew@gmail.com>
2025-06-28 21:15:25 +0300

    Renamed File

--- config/secrets.nix
+++ config/secrets.nix
@@ -8,7 +8,7 @@
     pkgs.writeTextFile {
       name = "hq-ca-chain.crt";
       destination = "/etc/ssl/certs/hq-ca-chain.crt";
-      text = builtins.readFile ../.env/secrets/public-keys/hq-ca-chain.crt;
+      text = builtins.readFile ../.env/secrets/certs/hq-ca-chain-public.crt;
     }
   ];
 }
