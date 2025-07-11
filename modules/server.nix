77b47df035e7f08efa8677659cc6d7801040dd62
Andrei Floroiu <floroiu.andrew@gmail.com>
2025-07-12 01:17:14 +0300

    Fix/f20250712 fixes (#28)
    * Fix default value
    
    * Fix

--- modules/server.nix
+++ modules/server.nix
@@ -64,7 +64,6 @@
       bantime = lib.mkDefault "1h";
       bantime-increment = {
         enable = lib.mkDefault true;
-        formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
         multipliers = "1 2 4 8 16 32 64";
         maxtime = "168h";
         overalljails = true;
