ded52b08519559338cb51ec1d6bbe4201fd6dfe6
Andrei Floroiu <floroiu.andrew@gmail.com>
2025-10-12 20:46:31 +0300

    Restrict custom lua for situations when dotnet SDK is installed

--- modules/nixvim/lua.nix
+++ modules/nixvim/lua.nix
@@ -1,6 +1,17 @@
-{ pkgs, ... }:
 {
-  programs.nixvim.extraConfigLua = ''
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  hasPackage =
+    pkg: builtins.any (p: p.pname or p.name or "" == pkg) config.environment.systemPackages;
+  hasDotnetSdk = hasPackage "dotnet" || hasPackage "dotnet-sdk" || hasPackage "dotnet-sdk_8";
+
+in
+{
+  programs.nixvim.extraConfigLua = lib.mkIf hasDotnetSdk ''
     local dap = require('dap')
     dap.adapters.coreclr = {
         type = 'executable',
