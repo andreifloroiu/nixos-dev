099a43c32715c693410774c93bf28cdda3e631f2
Andrei Floroiu <floroiu.andrew@gmail.com>
2025-11-11 15:26:25 +0200

    Remove programs from resurrect as they cause problems

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -91,7 +91,7 @@ in
         set -g @fzf-url-history-limit '2000'
 
         set -g @resurrect-strategy-nvim 'session'
-        set -g @resurrect-processes '~nvim ~mn ~lg ~lazygit ~mc'
+        set -g @resurrect-processes '~mc btop ~lj ~k9s'
         set -g @resurrect-capture-pane-contents 'on'
 
         set -g @continuum-restore 'on'
