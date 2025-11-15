e5573e46232d809fd7ecd5fcef7f221b53c6b3f5
Andrei Floroiu <floroiu.andrew@gmail.com>
2025-11-15 22:51:46 +0200

    Edited the Overlay

--- overlays/gemini-cli/default.nix
+++ overlays/gemini-cli/default.nix
@@ -1,3 +1,10 @@
-{ ... }:
-{
+final: prev: {
+  gemini-cli = prev.gemini-cli.overrideAttrs (old: {
+    src = prev.fetchFromGitHub {
+      owner = "google-gemini";
+      repo = "gemini-cli";
+      tag = "v15.3";
+      hash = "sha256-43ohCUqdM0quxwoN3s/3vOq4IRc+sck+BkAfqMEDq8g=";
+    };
+  });
 }
